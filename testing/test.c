6138e630b1ef81a4cba2781f98240d76f5c8a490
RajDaBest <imagination12344321.com@gmail.com>
2024-10-28 14:01:19 +0530

    fixed drag forces; minor bug fix

--- testing/test.c
+++ testing/test.c
@@ -2,19 +2,25 @@
 #include <SDL2/SDL.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>
 
 #define WINDOW_WIDTH 1920
 #define WINDOW_HEIGHT 1080
 #define CIRCLE_RADIUS 10
 #define MAX_PARTICLES 1000
+#define TARGET_FPS 60
+#define FRAME_TIME (1.0 / TARGET_FPS)
 
 typedef struct
 {
-    Particle particle;
+    Particle *particle;
     bool active;
 } ParticleInstance;
 
-void renderCircle(SDL_Renderer *renderer, int x, int y, int radius)
+// Global force parameters
+static DragCoefficients dragCoeffs = {0.05, 0.005};
+
+static void renderCircle(SDL_Renderer *renderer, int x, int y, int radius)
 {
     for (int w = 0; w < radius * 2; w++)
     {
@@ -30,78 +36,162 @@ void renderCircle(SDL_Renderer *renderer, int x, int y, int radius)
     }
 }
 
-void initializeParticle(Particle *particle)
+static Particle *createParticle(void)
 {
-    particle->position = vectorDef(CIRCLE_RADIUS, WINDOW_HEIGHT / 2, 0.0);
-    particle->velocity = vectorDef(600, 1000, 0.0);
-    particle->acceleration = vectorDef(0, -1200, 0.0);
-    setMass(particle, 1.0);
-    particle->damping = 0.99;
-    particle->forceAccum = vectorDef(0, -1200, 0.0);
+    // Random velocity variation for more interesting motion
+    real velX = 800.0 + (rand() % 50);
+    real velY = 600.0 + (rand() % 100);
+
+    Vector initPos = vectorDef(CIRCLE_RADIUS, WINDOW_HEIGHT / 2, 0.0);
+    Vector initVel = vectorDef(velX, velY, 0.0);
+    Vector initAcc = vectorDef(0.0, 0.0, 0.0); // Let forces handle acceleration
+
+    Particle *particle = Particle_Create(
+        initPos, // Initial position
+        initVel, // Initial velocity
+        initAcc, // Initial acceleration
+        2.0,     // Mass
+        0.9,    // Damping
+        0.0      // Start time
+    );
+
+    if (particle)
+    {
+        // Add gravity force (always active)
+        Particle_AddForce(particle, Particle_GravityForce, 0.0, INFINITY, NULL);
+
+        // Add drag force (always active)
+        Particle_AddForce(particle, Particle_DragForce, 0.0, INFINITY, &dragCoeffs);
+    }
+
+    return particle;
 }
 
-int main()
+static void cleanupParticles(ParticleInstance *particles, int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        if (particles[i].particle)
+        {
+            Particle_Destroy(particles[i].particle);
+            particles[i].particle = NULL;
+        }
+    }
+}
+
+int main(void)
 {
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        fprintf(stderr, "SDL initialization failed: %s\n", SDL_GetError());
         return 1;
-    SDL_Window *window = SDL_CreateWindow("Particle Simulation", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    }
+
+    SDL_Window *window = SDL_CreateWindow(
+        "Particle Simulation",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        WINDOW_WIDTH, WINDOW_HEIGHT,
+        SDL_WINDOW_SHOWN);
+
+    if (!window)
+    {
+        fprintf(stderr, "Window creation failed: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(
+        window, -1,
+        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    if (!renderer)
+    {
+        fprintf(stderr, "Renderer creation failed: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    // Initialize random seed
+    srand((unsigned int)time(NULL));
 
     ParticleInstance particles[MAX_PARTICLES] = {0};
     int particleCount = 0;
-
     bool running = true;
     SDL_Event event;
 
+    Uint32 lastTime = SDL_GetTicks();
+
     while (running)
     {
+        // Handle events
         while (SDL_PollEvent(&event))
         {
             if (event.type == SDL_QUIT)
-                running = false;
-            if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_a)
             {
-                if (particleCount < MAX_PARTICLES)
+                running = false;
+            }
+            else if (event.type == SDL_KEYDOWN)
+            {
+                if (event.key.keysym.sym == SDLK_a && particleCount < MAX_PARTICLES)
                 {
-                    initializeParticle(&particles[particleCount].particle);
-                    particles[particleCount].active = true;
-                    particleCount++;
+                    // Create new particle
+                    Particle *newParticle = createParticle();
+                    if (newParticle)
+                    {
+                        particles[particleCount].particle = newParticle;
+                        particles[particleCount].active = true;
+                        particleCount++;
+                    }
                 }
             }
         }
 
+        // Calculate delta time
+        Uint32 currentTime = SDL_GetTicks();
+        real deltaTime = 1.0 / 60.0;
+        lastTime = currentTime;
+
+        // Update physics
         for (int i = 0; i < particleCount; i++)
         {
-            if (particles[i].active)
+            if (particles[i].active && particles[i].particle)
             {
-                integrate(&particles[i].particle, 0.008); // Approx. 60 FPS
-                if (particles[i].particle.position.y < -CIRCLE_RADIUS)
+                Particle_Integrate(particles[i].particle, deltaTime);
+
+                // Check if particle is off screen
+                if (particles[i].particle->position.y < -CIRCLE_RADIUS)
                 {
-                    particles[i].active = false; // Deactivate particle if it goes off-screen
+                    particles[i].active = false;
+                    Particle_Destroy(particles[i].particle);
+                    particles[i].particle = NULL;
                 }
             }
         }
 
+        // Render
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
         for (int i = 0; i < particleCount; i++)
         {
-            if (particles[i].active)
+            if (particles[i].active && particles[i].particle)
             {
-                int screenX = (int)particles[i].particle.position.x;
-                int screenY = WINDOW_HEIGHT - (int)particles[i].particle.position.y;
+                int screenX = (int)particles[i].particle->position.x;
+                int screenY = WINDOW_HEIGHT - (int)particles[i].particle->position.y;
                 renderCircle(renderer, screenX, screenY, CIRCLE_RADIUS);
             }
         }
 
         SDL_RenderPresent(renderer);
-        SDL_Delay(8); // 16 ms delay for 60 FPS
     }
 
+    // Cleanup
+    cleanupParticles(particles, particleCount);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
+
     return 0;
 }
